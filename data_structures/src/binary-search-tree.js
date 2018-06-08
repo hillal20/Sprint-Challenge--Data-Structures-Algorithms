3040d29defee6c26591310d061c15dc1068254b4
hilal aissani <hilalaissani@gmail.com>
2018-06-08 17:02:12 -0400

    50

--- data_structures/src/binary-search-tree.js
+++ data_structures/src/binary-search-tree.js
@@ -7,11 +7,29 @@ class BinarySearchTree {
 
   depthFirstForEach(cb) {
     /* Your code here */
-    
+    cb(this.value);
+    if (this.left) {
+      this.left.depthFirstForEach(cb);
+    }
+    if (this.right) {
+      this.right.depthFirstForEach(cb);
+    }
   }
 
   breadthFirstForEach(cb) {
     /* Your code here */
+    const q = new Array();
+    // push our root node onto our queue
+    q.push(this);
+    // iterate through our queue until it is empty
+    while (q.length > 0) {
+      // dequeue the next node in line
+      const node = q.shift();
+      // add the node's children to the queue if they exists
+      if (node.left) q.push(node.left);
+      if (node.right) q.push(node.right);
+      cb(node.value);
+    }
 
   }
 
--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,6 +1,26 @@
+
+
+
 const heapsort = (arr) => {
-  /* Your code here */
-  
+
+  let result = []
+  if (arr.length === 0) {
+    return;
+  }
+  const heap = new Heap();
+
+  for (let i = 0; i < arr.length; i++) {
+    heap.insert(arr[i]);
+  }
+
+  while (heap.getSize() > 0) {
+
+    result.unshift(heap.delete());
+
+  }
+
+  return result;
+
 };
 
 
@@ -46,14 +66,14 @@ class Heap {
     const leftChildIndex = index * 2 + 1;
     const rightChildIndex = index * 2 + 2;
     let maxChild;
-    
+
     if (this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
       maxChild = this.storage[leftChildIndex] > this.storage[rightChildIndex] ? leftChildIndex : rightChildIndex;
     } else if (this.storage[leftChildIndex]) {
       maxChild = leftChildIndex;
     } else if (this.storage[rightChildIndex]) {
       maxChild = rightChildIndex;
-    } 
+    }
 
     if (this.storage[index] < this.storage[maxChild]) {
       [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
